e6ab5680b43496793f1dbb938485fd5813baa4ed
Vincent La <vincentla@Vincents-MacBook-Pro.local>
2017-08-07 15:28:35 -0700

    stubbing out functions

--- index.js
+++ index.js
@@ -17,6 +17,30 @@ class Game {
       el.innerHTML = '';
     })
   }
+
+  checkWin() {
+    //check for diagonal victory
+    //check for col victory
+    //check for row victory
+  }
+
+  diagonalVictory() {
+    //traverse the board
+      //for left diagnoal
+        //check all cells where index difference is 0
+      //for all right diagnoals
+        //check center and where row is 2/0 and col is 0/2
+  }
+
+  colVictory() {
+    //traverse the rows
+      //check if a single row has all of x or o
+  }
+
+  rowVictory() {
+    //traverse the col
+      //check if a single col has all of x or o
+  }
   
 }
 
