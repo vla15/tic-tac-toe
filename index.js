65e37d96a0b385f78a1b4927985704b5d03048b7
Vincent La <vincentla@Vincents-MacBook-Pro.local>
2017-08-07 17:26:17 -0700

    rendering board and accepting text input from user

--- index.js
+++ index.js
@@ -1,50 +1,42 @@
-class Game {
-  constructor(props) {
+var prompt = require('prompt');
+var board = [];
+var winner = false;
 
-  }
 
-  initialize() {
-    this.rowOne = document.getElementById("row1").children;
-    this.rowTwo = document.getElementById("row2").children;
-    this.rowThree = document.getElementById("row3").children;
-    this.clearBoard(this.rowOne);
-    this.clearBoard(this.rowTwo);
-    this.clearBoard(this.rowThree);
-  }
+var turn = 'x' || turn;
+var board = ['_', '_', '_', '_', '_', '_', '_', '_', '_']
+var player = 'player one' || player;
 
-  clearBoard(row) {
-    [...row].forEach(el => {
-      el.innerHTML = '';
-    })
-  }
+console.log(
+  `    A B C 
+  0|${board[0]}|${board[3]}|${board[6]}|
+  1|${board[1]}|${board[4]}|${board[7]}|
+  2|${board[2]}|${board[5]}|${board[8]}|`);
+prompt.start();
 
-  checkWin() {
-    //check for diagonal victory
-    //check for col victory
-    //check for row victory
-  }
 
-  diagonalVictory() {
-    //traverse the board
-      //for left diagnoal
-        //check all cells where index difference is 0
-      //for all right diagnoals
-        //check center and where row is 2/0 and col is 0/2
-  }
+var row = {
+  name: 'row',
+  message:`Please input your row ${player}: `
+}
 
-  colVictory() {
-    //traverse the rows
-      //check if a single row has all of x or o
-  }
-
-  rowVictory() {
-    //traverse the col
-      //check if a single col has all of x or o
-  }
-  
+var col = {
+  name: 'col',
+  message: `Please input your col ${player}: ` 
 }
 
 
-var game = new Game();
-
-game.initialize();
+prompt.get([row, col], (err, results) => {
+  if (err) {
+    return onErr(err);
+  }
+  var colResult
+  if (results.col.toLowerCase() === 'a') {
+    colResult = 0;
+  } else if (results.col.toLowerCase() === 'b') {
+    colResult = 1;
+  } else if (results.col.toLowerCase() === 'c') {
+    colResult = 2;
+  }
+  console.log(colResult);
+})
